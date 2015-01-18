24aae4609234317d27717388211aae06176e59f4
Matthias W. Smith <mwsmith2@uw.edu>
2015-01-18 14:09:54 -0800

    Wrote a data loader that parses into a dictionary of traits with a single column of array values for the data.

---
+++ src/loader.py
@@ -0,0 +1,37 @@
+import csv
+import numpy as np
+
+def load(filename, colname=None):
+
+	# We nned to get the total number of entries first
+	reader = csv.reader(open(filename), delimiter=',')
+	traits = reader.next()
+
+	npoints = 0
+	for line in reader:
+		npoints += len(line[1].split(' '))
+
+	# Set up the trait dictionary and 
+	res = {}
+	for trait in traits:
+		res[trait] = np.empty(npoints)
+
+	# Reopen the file. We need one more pass to store the data.
+	reader = csv.reader(open(filename), delimiter=',')
+
+	# Get the header out of the way.
+	traits = reader.next()
+
+	idx = 0
+	for line in reader:
+
+		# Figure out how many points are in this entry
+		size = np.array(line[1].split(' ')).shape[0]
+
+		for j, trait in enumerate(traits):
+
+			res[trait][idx:idx+size] = np.array(line[j].split(' '))
+
+		idx += size
+
+	return res
