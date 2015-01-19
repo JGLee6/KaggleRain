4813381740fb590b8278898c5a6ff5e37e68f816
Matthias W. Smith <mwsmith2@uw.edu>
2015-01-18 16:10:15 -0800

    Added the errors present in radar quality data to generate valid data checks, and corrected values computed for Kdp which had errors present.

--- src/loader.py
+++ src/loader.py
@@ -1,6 +1,9 @@
 import csv
 import numpy as np
 
+error_lo = -98999.0
+error_hi = 998.0
+
 def load(filename, colname=None):
 
 	# We nned to get the total number of entries first
@@ -37,18 +40,25 @@ def load(filename, colname=None):
 	# We need to set Kdp ourselves for some stupid reason.
 	res['Kdp'] = np.log(np.abs(res['RR3']) / 40.6) / 0.866
 
+	# Now reset the values that had errors (and nans to be sure).
+	args = (res['RR3'] == np.nan)
+	args |= (res['RR3'] < error_lo)
+	res['Kdp'][args] = res['RR3'][args]
+
 	return res
 
 def clean(array):
 
-	return array[(array > -99000.0) & (array != np.nan)]
+	return array[(array > error_lo) & (array < error_hi) & (array != np.nan)]
 
 
 def common_indices(array1, array2):
 
-	indices = array1 > -99000.0
+	indices = array1 > error_lo
+	indices = array1 < error_hi
 	indices &= array1 != np.nan
-	indices &= array2 > -99000.0 
+	indices &= array2 > error_lo 
+	indices &= array2 > error_hi 
 	indices &= array2 != np.nan
 
 	return indices
